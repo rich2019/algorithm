64aa7b40e2e517bfdbf79a848cd560a64ebc5f90
cdd <1327571308@qq.com>
2021-11-26 22:29:05 +0800

    init

---
+++ letcode/src/main/java/jian_zhi_offer/CQueue.java
@@ -0,0 +1,22 @@
+package jian_zhi_offer;
+
+public class CQueue {
+
+    public CQueue() {
+
+    }
+
+    public void appendTail(int value) {
+
+    }
+
+    public int deleteHead() {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
