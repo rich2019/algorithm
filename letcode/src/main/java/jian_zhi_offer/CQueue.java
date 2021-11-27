f5686d91d639e4a01f84d24006684518fa13597f
cdd <1327571308@qq.com>
2021-11-27 22:41:37 +0800

    两个栈实现队列

--- letcode/src/main/java/jian_zhi_offer/CQueue.java
+++ letcode/src/main/java/jian_zhi_offer/CQueue.java
@@ -1,22 +1,52 @@
 package jian_zhi_offer;
 
+import java.util.Iterator;
+import java.util.Stack;
+
+/**
+ * 剑指 Offer 09. 用两个栈实现队列
+ * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。
+ * (若队列中没有元素，deleteHead 操作返回 -1 )
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：
+ * ["CQueue","appendTail","deleteHead","deleteHead"]
+ * [[],[3],[],[]]
+ * 输出：[null,null,3,-1]
+ * 示例 2：
+ * <p>
+ * 输入：
+ * ["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+ * [[],[],[5],[2],[],[]]
+ * 输出：[null,-1,null,null,5,2]
+ */
 public class CQueue {
+
+    private Stack<Integer> stackIn = new Stack<>();
+    private Stack<Integer> stackOut = new Stack<>();
 
     public CQueue() {
 
     }
 
     public void appendTail(int value) {
-
+        stackIn.push(value);
     }
 
     public int deleteHead() {
-
-        return 0;
-    }
-
-    public static void main(String[] args) {
-
+        Iterator<Integer> iterator = stackIn.iterator();
+        if (stackOut.isEmpty()) {
+            while (!stackIn.isEmpty()) {
+                stackOut.push(stackIn.pop());
+            }
+        }
+        if (stackOut.isEmpty()) {
+            return -1;
+        }
+        return stackOut.pop();
     }
 
 }
